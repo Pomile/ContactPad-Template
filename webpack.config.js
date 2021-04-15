57d25afce020c4fc179ee30e12003f525ff9d051
Babatunde <soft-sky@live.co.uk>
2021-04-15 14:50:59 +0100

    Setup
    -[x] Add npm package
    -[x] Setup babel compiler
    -[x] Setup webpack
    -[x] Setup tailwind

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './index.js',
+  },
+  devtool: devMode ? 'inline-source-map' : '',
+  devServer: {
+    contentBase: __dirname,
+    port: 9090,
+  },
+  optimization: {
+    moduleIds: 'named',
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: devMode ? '[name].bundle.[ext]' : '[hash].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Home',
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+
+    devMode ? new webpack.HotModuleReplacementPlugin() : '',
+  ],
+};
